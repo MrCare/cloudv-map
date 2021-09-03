bd01e4bf83398dbd09900c0eee01b191eb444359
MacCar <534192336car@gmail.com>
2021-09-03 13:03:37 +0800

    first commit

--- .eslintrc.js
+++ .eslintrc.js
@@ -13,6 +13,15 @@ module.exports = {
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-iterator': 0,
+    'no-restricted-syntax': 1,
+    indent: ['error', 4],
+    'import/extensions': ['error', 'ignorePackages', {
+      js: 'always',
+      mjs: 'never',
+      jsx: 'never',
+    }],
+    'no-plusplus': ['error', { allowForLoopAfterthoughts: true }],
   },
   overrides: [
     {
