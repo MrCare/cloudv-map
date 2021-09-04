87262e5db0ae11d068c132d7499539426521addc
MacCar <534192336car@gmail.com>
2021-09-05 07:52:22 +0800

    fix webpack problem

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,37 +1,37 @@
 module.exports = {
-  root: true,
-  env: {
-    node: true,
-  },
-  extends: [
-    'plugin:vue/essential',
-    '@vue/airbnb',
-  ],
-  parserOptions: {
-    parser: 'babel-eslint',
-  },
-  rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-iterator': 0,
-    'no-restricted-syntax': 1,
-    indent: ['error', 4],
-    'import/extensions': ['error', 'ignorePackages', {
-      js: 'always',
-      mjs: 'never',
-      jsx: 'never',
-    }],
-    'no-plusplus': ['error', { allowForLoopAfterthoughts: true }],
-  },
-  overrides: [
-    {
-      files: [
-        '**/__tests__/*.{j,t}s?(x)',
-        '**/tests/unit/**/*.spec.{j,t}s?(x)',
-      ],
-      env: {
-        jest: true,
-      },
+    root: true,
+    env: {
+        node: true,
     },
-  ],
+    extends: [
+        'plugin:vue/essential',
+        '@vue/airbnb',
+    ],
+    parserOptions: {
+        parser: 'babel-eslint',
+    },
+    rules: {
+        'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+        'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+        'no-iterator': 0,
+        'no-restricted-syntax': 1,
+        indent: ['error', 4],
+        'import/extensions': ['error', 'ignorePackages', {
+            js: 'always',
+            mjs: 'never',
+            jsx: 'never',
+        }],
+        'no-plusplus': ['error', { allowForLoopAfterthoughts: true }],
+    },
+    overrides: [
+        {
+            files: [
+                '**/__tests__/*.{j,t}s?(x)',
+                '**/tests/unit/**/*.spec.{j,t}s?(x)',
+            ],
+            env: {
+                jest: true,
+            },
+        },
+    ],
 };
--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import Vue from 'vue';
-import App from './app.vue';
-import { router } from './router/index.js';
-import store from './store/index.js';
+import App from '@/app.vue';
+import { router } from '@/router/index.js';
+import store from '@/store/index.js';
 
 import '@/assets/css/index.css';
 
