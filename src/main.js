fa0b1a1ce4ecf0fe85daa58a500a97698c4f8651
MacCar <534192336car@gmail.com>
2021-09-07 00:30:46 +0800

    add

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import ElementUI from 'element-ui';
 import Vue from 'vue';
 import VueRouter from 'vue-router';
 import { router } from '@/router/index.js';
-import App from './app.vue';
+import App from '@/app.vue';
 
 Vue.use(ElementUI);
 Vue.use(VueRouter);
